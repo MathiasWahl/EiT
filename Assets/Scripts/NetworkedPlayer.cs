a373490b157caa9a9eeee6464e0a6f6f7393f26a
Jonas Gyllensten <jonasgy@ntnu.no>
2020-02-26 16:21:40 +0100

    Add assets for multiplayer

---
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkedPlayer : Photon.MonoBehaviour
+{
+    public GameObject avatar;
+    public Transform playerGlobal, playerLocal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photonView.isMine) {
+        Debug.Log("Playerjoined");
+        playerGlobal = GameObject.Find("[CameraRig]").transform;
+        playerLocal = playerGlobal.Find("Camera");
+    
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
