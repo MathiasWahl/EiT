36cfb87cb42b2610ae4f24da6ce5ed02265a14c5
Mathias <ma10asw@gmail.com>
2020-03-11 19:04:05 +0100

    add online cube and broken spawn script

---
+++ Assets/Scripts/Multiplayer/SpawnCubes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCubes : MonoBehaviour
+{
+
+    public GameObject cubePrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        var lDevice = SteamVR_Controller.Input((int)ViveManager.Instance.leftHand.GetComponent<SteamVR_TrackedObject>().index);
+        var rDevice = SteamVR_Controller.Input((int)ViveManager.Instance.rightHand.GetComponent<SteamVR_TrackedObject>().index);
+ 
+        if (lDevice.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) 
+        {
+            PhotonNetwork.Instantiate(cubePrefab.name, ViveManager.Instance.leftHand.tranform.position, ViveManager.Instance.leftHand.tranform.rotation);
+        }
+
+        if (rDevice.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) 
+        {
+            PhotonNetwork.Instantiate(cubePrefab.name, ViveManager.Instance.rightHand.tranform.position, ViveManager.Instance.rightHand.tranform.rotation);
+        }
+
+
+    }
+}
