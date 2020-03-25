bbd910ec12fc18127e5969236c30c6cd05445422
Mathias Wahl <ma10asw@gmail.com>
2020-03-25 14:41:42 +0100

    add physical menus and stuff

--- Scripts/MenuButtonActions.cs
+++ Scripts/MenuButtonActions.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuButtonActions : MonoBehaviour
+public class MenuButtonActions : Photon.MonoBehaviour
 {
 
     public GameObject spawnObject;
- 
-    public void ScenesButton()
+    public Transform cameraRigTransform;
+    public Vector3 model_position = new Vector3(590, 2.96f, -18);
+    public Vector3 office_position = new Vector3(0, 0, 0);
+
+
+    public void TeleportToModelButton()
     {
         Debug.Log("Scenes work");
+        // get camera rig, change position
+        cameraRigTransform.position = model_position;
     }
 
-    public void ObjectsButton()
+    public void SpawnCubeButton()
     {
-        Instantiate(spawnObject);
+        PhotonNetwork.Instantiate("Online Cube", new Vector3(0, 0, 0), Quaternion.identity, 0);
+        spawnObject.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.player.ID);
+    }
+
+    public void TeleportToOfficeButton()
+    {
+        cameraRigTransform.position = office_position;
     }
 }
